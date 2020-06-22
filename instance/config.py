b15ff2fed0ed3ceca2f9cb05470ea216983f076d
Ogheneyoma Obomate Okobiah <32799642+Yomdroid@users.noreply.github.com>
2020-06-22 10:47:25 +0100

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,5 @@
+import os
+
 SECRET_KEY = 'qCJs2OkujX5Iz5VJSPbxHg'
-SQLALCHEMY_DATABASE_URI = 'mysql://analog:dt2016@localhost/analog_data'
+#SQLALCHEMY_DATABASE_URI = 'mysql://analog:dt2016@localhost/analog_data'
+SQLALCHEMY_DATABASE_URI = os.environ.get("CLEARDB_DATABASE_URL")
